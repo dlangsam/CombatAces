c0e44ae27e981420ff6e70c847588a191b1085f3
Devorah Langsam <devorah@langsam.org>
2016-11-06 09:55:43 -0500

    Connect movie title to modal

--- CombatAces/app/movies.js
+++ CombatAces/app/movies.js
@@ -9,6 +9,24 @@
     	};
     });
 
+    app.directive("movieModal", function(){
+      return{
+        restrict: 'E',
+        templateUrl: '../views/movie-modal.html',
+        controller: function(){
+            this.current = 0;
+            this.setCurrent = function(current){
+              this.current = current || 0;
+            };
+            this.isSet = function(current){
+              return this.current === current;
+            };
+          
+        },
+        controllerAs: 'myModal'
+      };
+    });
+
  
 
 })();
